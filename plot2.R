a5356f345cde608751ef391c0ed73c15f454adeb
Vaibhav <vaibhavt@buffalo.edu>
2015-11-07 18:32:38 -0500

    files added

---
+++ plot2.R
@@ -0,0 +1,27 @@
+filename <- "C:/Users/vtummalapalli/Desktop/data r/household_power_consumption.txt"
+data <- read.table(filename,
+                   header = TRUE,
+                   sep = ";",
+                   colClasses = c("character", "character", rep("numeric",7)),
+                   na = "?")
+dim(data) # 2075259 9
+attach(data)
+## We only need data of 2 days.
+subset <- Date == "1/2/2007" | Date == "2/2/2007"
+newData <- data[subset, ]
+attach(newData)
+x <- paste(Date, Time)
+newData$DateTime <- strptime(x, "%d/%m/%Y %H:%M:%S")
+rownames(newData) <- 1:nrow(newData)
+dim(newData) # 2880   10
+attach(newData)
+
+#plot2 
+png(filename = "plot2.png", 
+    width = 480, height = 480,
+    units = "px", bg = "transparent")
+plot(DateTime, Global_active_power, 
+     type = "l",
+     xlab = "",
+     ylab = "Global Active Power (kilowatts)")
+dev.off()
